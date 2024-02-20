86ea55e25284d42f7b4e211b92bf378faed1aa4b
Raşit Çolakel <rasitcolakel@hotmail.com>
2024-02-21 00:36:32 +0300

    Update Navbar styling to include responsive padding

--- src/components/layouts/navbar/index.tsx
+++ src/components/layouts/navbar/index.tsx
@@ -7,7 +7,7 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div className="flex justify-between items-center h-16 border-b border-gray-200  dark:border-gray-800 w-full">
+    <div className="px-3 flex justify-between items-center h-16 border-b border-gray-200  dark:border-gray-800 w-full md:px-0">
       <div className="flex items-center space-x-4 flex-1 w-full">
         <Avatar>
           <AvatarImage src="./pp.jpg" alt="@shadcn" />
